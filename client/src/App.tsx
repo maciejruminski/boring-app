fca52fb029976d459e19dfaac8dc0597fccdb945
Maciej Rumiński <maciejruminski91@gmail.com>
2021-11-05 14:33:06 +0100

    Adding testing request to API

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [data, setData] = React.useState({ data: null });
+
+  const callBackendAPI = async () => {
+    const response = await fetch("/express_backend");
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message);
+    }
+    return body;
+  };
+
+  React.useEffect(() => {
+    callBackendAPI()
+      .then((res) => setData({ data: res.express }))
+      .then((res) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
