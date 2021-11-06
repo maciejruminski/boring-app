1d00e3b8a013a0c41d5c595e287dff858975c407
Maciej Rumiński <maciejruminski91@gmail.com>
2021-11-06 12:50:20 +0100

    Importing Dashboard and SignUp components to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Dashboard from "./components/Dashboard";
+import SignUp from "./components/SignUp";
 
 function App() {
-  const [data, setData] = React.useState({ data: null });
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
 
-  const callBackendAPI = async () => {
-    const response = await fetch("/express_backend");
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      throw Error(body.message);
-    }
-    return body;
-  };
-
-  React.useEffect(() => {
-    callBackendAPI()
-      .then((res) => setData({ data: res.express }))
-      .then((res) => console.log(data))
-      .catch((err) => console.log(err));
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">{isLoggedIn ? <Dashboard /> : <SignUp />}</div>;
 }
 
 export default App;
