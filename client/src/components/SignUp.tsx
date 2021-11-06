5789221e501dbf4f1a7d2ded85536530327e116c
Maciej Rumiński <maciejruminski91@gmail.com>
2021-11-06 06:33:42 +0100

    Adding initial SignUp component

---
+++ client/src/components/SignUp.tsx
@@ -0,0 +1,11 @@
+import Email from "./Email";
+import OneTimePassword from "./OneTimePassword";
+
+export default function SignUp() {
+  return (
+    <>
+      <Email />
+      <OneTimePassword />
+    </>
+  );
+}
