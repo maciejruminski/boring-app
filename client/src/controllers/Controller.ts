015d4910337424c558e51410d29f4912a9cd2eda
Maciej Rumiński <maciejruminski91@gmail.com>
2021-11-07 19:25:13 +0100

    Adding setUser method

--- client/src/controllers/Controller.ts
+++ client/src/controllers/Controller.ts
@@ -3,14 +3,18 @@ import LocalStorageController from "./LocalStorageController";
 import ApiController from "./ApiController";
 
 export default class Controller {
+  private static setUser(userUUID: string): void {
+    LocalStorageController.setUserUUID(userUUID);
+    LocalStorageController.setUserAuthentication();
+  }
+
   static checkIfLoggedIn(): boolean {
     return LocalStorageController.getUserAuthentication() ? true : false;
   }
 
   static async verifyOneTimePassword(
-    oneTimePassword: string,
-    checkIfLoggedIn: Function
-  ): Promise<void> {
+    oneTimePassword: string
+  ): Promise<boolean> {
     const response = await ApiController.verifyOneTimePassword(oneTimePassword);
     const statusIsNotOk = response.status !== 200;
 
@@ -18,9 +22,8 @@ export default class Controller {
       throw Error(response.errorMessage);
     }
 
-    LocalStorageController.setUserUUID(response.uuid);
-    LocalStorageController.setUserAuthentication();
+    this.setUser(response.uuid);
 
-    checkIfLoggedIn();
+    return true;
   }
 }
