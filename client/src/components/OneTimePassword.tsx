6e128d10495504f3e72bb9cc88d4f0a7c5cc4831
Maciej Rumiński <maciejruminski91@gmail.com>
2021-11-07 13:51:28 +0100

    Adding verifyOneTimePassword function to form on submit and cleaning a code a little bit

--- client/src/components/OneTimePassword.tsx
+++ client/src/components/OneTimePassword.tsx
@@ -1,61 +1,34 @@
-import React from "react";
+// Functions.
+import { useRef } from "react";
+
+// Context.
+import { useGlobalContext } from "../context";
 
 export default function OneTimePassword() {
-  const [oneTimePassword, setOneTimePassword] = React.useState("");
-
-  const callBackendAPI = async () => {
-    const response = await fetch("/verify", {
-      method: "POST",
-      body: JSON.stringify({
-        oneTimePassword: oneTimePassword,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      console.log("errors message");
-      throw Error(body.message);
-    }
-
-    return body;
-  };
-
-  const handleChange = (evt: any) => {
-    setOneTimePassword(evt.target.value);
-  };
-
-  const handleSubmit = (evt: any) => {
-    evt.preventDefault();
-    console.log("before call");
-    callBackendAPI()
-      .then((res) => console.log("res", res))
-      .catch((err) => console.log("err", err));
-  };
+  const { actions } = useGlobalContext();
+  const oneTimePasswordRef = useRef<HTMLInputElement>(null);
+  const verifyOneTimePassword = (evt: React.FormEvent<HTMLFormElement>) =>
+    actions.verifyOneTimePassword(evt, oneTimePasswordRef?.current?.value);
 
   return (
-    <div>
-      <form method="POST" onSubmit={handleSubmit}>
-        <p>
-          <label>Enter otp</label>
-          <input
-            type="text"
-            name="oneTimePassword"
-            required
-            onChange={handleChange}
-          />
-        </p>
-        <p className="full">
-          <button type="submit">Submit</button>
-        </p>
+    <>
+      <form method="POST" onSubmit={verifyOneTimePassword}>
+        <label>Enter otp</label>
+        <input
+          type="text"
+          name="oneTimePassword"
+          required
+          ref={oneTimePasswordRef}
+        />
+
+        <button type="submit">Submit</button>
       </form>
-      <br />
-      <form method="POST" action="resend">
+
+      {/* <form method="POST" action="resend">
         <p className="full">
           <button type="submit">resend otp</button>
         </p>
-      </form>
-    </div>
+      </form> */}
+    </>
   );
 }
