09725ca11b00095bca0ec2ecfef2aa55e9813171
Maciej Rumiński <maciejruminski91@gmail.com>
2021-11-25 07:10:07 +0100

    Adding addUserToDatabase method to ApiController

--- client/src/controllers/ApiController.ts
+++ client/src/controllers/ApiController.ts
@@ -1,6 +1,16 @@
 export default class ApiController {
-  static async verifyOneTimePassword(oneTimePassword: any) {
-    const response = await fetch("/verify-one-time-password", {
+  static async addUserToDatabase(userUUID: string) {
+    const response = await fetch("/add-user", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ userUUID }),
+    });
+
+    return await response.json();
+  }
+
+  static async verifyOneTimePassword(oneTimePassword: string) {
+    const response = await fetch("/verify-password", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ oneTimePassword }),
