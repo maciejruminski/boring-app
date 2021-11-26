f4dc5020e864d15ebbf0a8bf6638d27c37768ab8
Maciej Rumiński <maciejruminski91@gmail.com>
2021-11-26 10:17:13 +0100

    Moving Filters component to Filters directory and renaming a file to index

---
+++ client/src/components/Filters/index.tsx
@@ -0,0 +1,12 @@
+// Components.
+import Form from "./Form";
+import Preview from "./Preview";
+
+export default function Filters(): JSX.Element {
+  return (
+    <>
+      <Form />
+      <Preview />
+    </>
+  );
+}
