b5e3611c4c1af6889fe9a52794ab8d5d39425abf
Maciej Rumiński <maciejruminski91@gmail.com>
2021-11-26 10:12:16 +0100

    Refactoring Filters component

--- client/src/components/Filters.tsx
+++ client/src/components/Filters.tsx
@@ -1,34 +1,12 @@
-// Functions.
-import { useEffect } from "react";
-
-// Context.
-import { useGlobalContext } from "../context";
-
 // Components.
 import Form from "./Filters/Form";
+import Preview from "./Filters/Preview";
 
 export default function Filters() {
-  const { state, actions } = useGlobalContext();
-
-  useEffect(() => actions.filter(state.filters.types), []);
-
-  console.log(state.filters);
-
   return (
-    <div>
-      <button onClick={() => actions.setFiltersModalOn()}>Pokaż filtry</button>
-
+    <>
       <Form />
-
-      <div>
-        <h3>Filtry:</h3>
-        <p>Distance: {state.filters.types.distance}</p>
-        <p>Keyword: {state.filters.types.keyword}</p>
-        <p>Type: {state.filters.types.type}</p>
-        <p>Min Price: {state.filters.types.minPrice}</p>
-        <p>Max Price: {state.filters.types.maxPrice}</p>
-        <p>Open Now: {state.filters.types.openNow ? "true" : "false"}</p>
-      </div>
-    </div>
+      <Preview /> 
+    </>
   );
 }
---
+++ client/src/components/Filters/Form.tsx
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+
+// Functions.
+import { useRef } from "react";
+
+// Context.
+import { useGlobalContext } from "../../context";
+
+// Components.
+import Types from "./Types";
+import Keyword from "./Keyword";
+import Distance from "./Distance";
+import MinPrice from "./MinPrice";
+import MaxPrice from "./MaxPrice";
+import OpenNow from "./OpenNow";
+
+const SForm = styled.form<{ isModalOpen: boolean }>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: ${({ isModalOpen }) => (isModalOpen ? "1" : "0")};
+  transform: ${({ isModalOpen }) =>
+    isModalOpen ? "translateY(0)" : "translateY(-100%)"};
+  transition: opacity 0.3s ease-out, transform 0.3s ease-out;
+  background-color: black;
+  color: white;
+`;
+
+export default () => {
+  const { state, actions } = useGlobalContext();
+  const distanceRef = useRef<HTMLInputElement>(null);
+  const keywordRef = useRef<HTMLInputElement>(null);
+  const typeRef = useRef<HTMLSelectElement>(null);
+  const minPriceRef = useRef<HTMLSelectElement>(null);
+  const maxPriceRef = useRef<HTMLSelectElement>(null);
+  const openNowRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <>
+      <button onClick={() => actions.setFiltersModalOn()}>Pokaż filtry</button>
+
+      <SForm
+        isModalOpen={state.filters.isModalOpen}
+        method="POST"
+        onSubmit={(evt: React.FormEvent<HTMLFormElement>) => {
+          evt.preventDefault();
+          actions.filter({
+            distance: distanceRef?.current?.value,
+            keyword: keywordRef?.current?.value,
+            type: typeRef?.current?.value,
+            minPrice: minPriceRef?.current?.value,
+            maxPrice: maxPriceRef?.current?.value,
+            openNow: openNowRef?.current?.checked,
+          });
+        }}
+      >
+        <Distance ref={distanceRef} />
+        <Keyword ref={keywordRef} />
+        <Types ref={typeRef} />
+        <MinPrice ref={minPriceRef} />
+        <MaxPrice ref={maxPriceRef} />
+        <OpenNow ref={openNowRef} />
+
+        <button
+          type="submit"
+          className="btn btn-primary btn-sm"
+          onClick={() => actions.setFiltersModalOff()}
+        >
+          Submit
+        </button>
+      </SForm>
+    </>
+  );
+};
