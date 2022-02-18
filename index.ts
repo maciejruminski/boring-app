de4001bce5c7ae5dd8fa81da7e64babfdb60b1ac
Maciej Rumiński <maciejruminski91@gmail.com>
2022-02-18 08:10:50 +0100

    Updating index.ts file

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 // Libraries.
-import express from "express";
-import dotenv from "dotenv";
+import * as express from "express";
+import * as dotenv from "dotenv";
+import * as path from "path";
+
+// Config.
+import config from "./config";
 
 // Environment variables.
 dotenv.config();
@@ -11,6 +15,8 @@ import oneTimePasswordRouter from "./routes/one-time-password";
 import googleSheetsRouter from "./routes/google-sheets";
 import googlePlacesRouter from "./routes/google-places";
 // import { googlePlacesRouter,... }from "./routes";
+
+
 
 // Creates an Express application.
 const app = express();
@@ -26,5 +32,14 @@ app.use(express.static("public"));
 app.use(oneTimePasswordRouter);
 app.use(googleSheetsRouter);
 app.use(googlePlacesRouter);
+
+// For Heroku.
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+});
+
+app.listen(config.port, () => `App is live at ${config.port}`);
 
 export default app;
