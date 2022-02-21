fd47fb2520e61885dfd5e075e020a96dc0081f1d
Maciej Rumiński <maciejruminski91@gmail.com>
2022-02-21 08:26:27 +0100

    Adding serviceWorkerRegistration file

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import AuthContextProvider from "./context/Auth/AuthProvider";
 import "./i18n/index";
