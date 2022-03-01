d629da4afe53f12fd74afd6662ef08e1a32f4d53
Maciej Rumiński <maciejruminski91@gmail.com>
2022-03-01 15:27:09 +0100

    Making 'bar' as default value for types filters

--- client/src/context/FiltersAndPlaces/initialState.ts
+++ client/src/context/FiltersAndPlaces/initialState.ts
@@ -3,7 +3,7 @@ const initialState: IFiltersAndPlacesState = {
   filters: {
     distance: "1000",
     keyword: "",
-    type: "amusement_park",
+    type: "bar",
     minPrice: "_",
     maxPrice: "_",
     openNow: false,
