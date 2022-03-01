ba10bc9570a1f73f531dc046c91c4a723f830fcd
Maciej Rumiński <maciejruminski91@gmail.com>
2022-03-01 18:33:46 +0100

    Refactoring tests a little

--- client/cypress/integration/tests.js
+++ client/cypress/integration/tests.js
@@ -1,44 +1,32 @@
 // Language.
 import i18n from "../../src/i18n";
 
-const findPlacesByFilters = (distance, type) => {
-  return cy
-    .findByTestId("showFilters")
-    .click()
-    .then(() => {
-      cy.findByTestId("distance").select(distance);
-      cy.findByTestId("types").select(type);
-      cy.findByTestId("filter").click({ force: true });
-    });
-};
-
 describe("Login", () => {
   it("Email checking", () => {
     cy.visit("http://localhost:3000");
 
-    // let errorText;
+    let errorText;
 
-    // // Empty email
-    // errorText = /please fill in this field\./i;
-    // cy.findByTestId("emailSubmit").click();
+    // The user clicks on button without filling an input.
+    errorText = /please fill in this field\./i;
+    cy.findByTestId("emailSubmit").click();
 
-    // cy.findByText(errorText);
+    cy.findByText(errorText);
 
-    // // Wrong email;
-    // errorText =
-    //   /please include an '@' in the email address\. 'wrong\-email\.gmail\.com' is missing an '@'\./i;
-    // cy.findByTestId("email").type("wrong-email.gmail.com");
+    // After receiving an error the user types an email but makes some mistakes.
+    errorText =
+      /please include an '@' in the email address\. 'wrong\-email\.gmail\.com' is missing an '@'\./i;
+    cy.findByTestId("email").type("wrong-email.gmail.com");
 
-    // cy.findByText(errorText);
+    cy.findByText(errorText);
 
-    // // Email with multiple @ sing.
-    // errorText = /a part following '@' should not contain the symbol '@'\./i;
-    // cy.findByTestId("email").clear();
-    // cy.findByTestId("email").type("multiple@@@gmail.com");
+    errorText = /a part following '@' should not contain the symbol '@'\./i;
+    cy.findByTestId("email").clear();
+    cy.findByTestId("email").type("multiple@@@gmail.com");
 
-    // cy.findByText(errorText);
+    cy.findByText(errorText);
 
-    // Success.
+    // After some tries, the user finally types valid email.
     cy.findByTestId("email").clear();
     cy.findByTestId("email").type("good-email@gmail.com");
     cy.findByTestId("email-error").should("not.exist");
@@ -46,38 +34,39 @@ describe("Login", () => {
   });
 
   it("Password checking", () => {
-    // let errorText;
+    let errorText;
 
-    // // Resend password
-    // const successText = /password has been sent!/i;
-    // cy.findByRole("button", { name: /resend one\-time password/i }).click();
+    // The user has problem and can't find an email with one-time password,
+    // so he clicks on resend button.
+    const successText = /password has been sent!/i;
+    cy.findByRole("button", { name: /resend one\-time password/i }).click();
 
-    // cy.findByText(successText);
+    cy.findByText(successText);
 
-    // // Empty password
-    // errorText = /please fill in this field\./i;
-    // cy.findByRole("button", { name: /login/i }).click();
+    // The user clicks on button without filling an input.
+    errorText = /please fill in this field\./i;
+    cy.findByRole("button", { name: /login/i }).click();
 
-    // cy.findByText(errorText);
+    cy.findByText(errorText);
 
-    // // Too short password
-    // errorText = /password must contain exactly 6 digits\./i;
-    // cy.findByTestId("signUpPassword").clear();
-    // cy.findByTestId("signUpPassword").type("123");
-    // cy.findByRole("button", { name: /login/i }).click();
+    // The user types too short password.
+    errorText = /password must contain exactly 6 digits\./i;
+    cy.findByTestId("signUpPassword").clear();
+    cy.findByTestId("signUpPassword").type("123");
+    cy.findByRole("button", { name: /login/i }).click();
 
-    // cy.findByText(errorText);
+    cy.findByText(errorText);
 
-    // // Wrong password
-    // const wrongPassword = "abc123";
+    // The user types wrong password.
+    const wrongPassword = "abc123";
 
-    // cy.findByTestId("signUpPassword").clear();
-    // cy.findByTestId("signUpPassword").type(wrongPassword);
-    // cy.findByRole("button", { name: /login/i }).click();
+    cy.findByTestId("signUpPassword").clear();
+    cy.findByTestId("signUpPassword").type(wrongPassword);
+    cy.findByRole("button", { name: /login/i }).click();
 
-    // cy.findByText(/password is incorrect, please double check your email\./i);
+    cy.findByText(/password is incorrect, please double check your email\./i);
 
-    // Valid password
+    // After some tries, the user finally types valid password.
     const validPassword = "$ecret";
 
     cy.findByTestId("signUpPassword").clear();
@@ -111,9 +100,14 @@ describe("Filters", () => {
       });
     };
 
-    // checkIfListExists("1000", "amusement_park");
-    // checkIfListExists("650", "bar");
-    // checkIfListExists("15000", "library");
+    // The user searches for parks within 1km.
+    checkIfListExists("1000", "amusement_park");
+
+    // Then the user tries searching for bars within 650m.
+    checkIfListExists("650", "bar");
+
+    // Finally the user tries searching for libraries within 15km.
+    checkIfListExists("15000", "library");
   });
 });
 
@@ -198,4 +192,3 @@ describe("Historic Places", () => {
     });
   });
 });
-
